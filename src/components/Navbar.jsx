3982a39b3c13db32e8d68b75d93056a2d9a82201
shahbozshirinboyev <shahboz.sh.b@gmail.com>
2024-09-22 08:53:56 +0500

    Add Logo

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -3,6 +3,9 @@ import { useState } from "react";
 import { NavbarMenu } from "../data/data";
 import { NavLink } from "react-router-dom";
 
+// logo
+import logo from '../../public/logo/art-old-wood-e.png'
+
 // ResponsiveMenu
 import ResponsiveMenu from './ResponsiveMenu'
 
@@ -16,12 +19,13 @@ function Navbar() {
   return (
     <>
     {/* container flex justify-between items-center py-8 border border-green-700 */}
-      <div className="container flex justify-between items-center border-[5px] border-purple-700 py-8">
+      <div className="container flex justify-between gap-1 items-center border-[5px] border-purple-700 py-8">
         {/* logo section */}
         <div className="border border-red-700 text-xl flex items-center gap-2 font-bold uppercase">
-          <i className="bi bi-braces-asterisk"></i>
+          {/* <i className="bi bi-braces-asterisk"></i>
           <p>Coders</p>
-          <p className="text-secondary">Gym</p>
+          <p className="text-secondary">Gym</p> */}
+          <img src={logo} alt="" className="w-[180px]"/>
         </div>
         {/* Menu section */}
         <div className="hidden md:block border border-red-700">
@@ -35,7 +39,7 @@ function Navbar() {
         </div>
         {/* Icons section */}
         <div className=" hidden md:hidden lg:hidden xl:flex items-center gap-2 border border-red-700">
-          <button className="hover:bg-primary text-primary font-semibold hover:text-white rounded-md border-2 border-primary px-6 py-2 duration-200">
+          <button className="hover:bg-primary whitespace-nowrap text-primary font-semibold hover:text-white rounded-md border-2 border-primary px-6 py-2 duration-200">
           <i class="bi bi-telephone"></i>
           <span className="ml-2">+998 93 456 34 21</span>
           </button>
