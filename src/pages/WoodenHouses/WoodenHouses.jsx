026735d9101a758e1043d214feb86dfa9737758d
shahbozshirinboyev <shahboz.sh.b@gmail.com>
2024-10-07 17:19:31 +0500

    change woodenhouses

--- src/pages/WoodenHouses/WoodenHouses.jsx
+++ src/pages/WoodenHouses/WoodenHouses.jsx
@@ -4,22 +4,25 @@ import ScrollableMenu from "../../components/ScrollableMenu";
 import { motion } from "framer-motion";
 
 function WoodenHouses() {
-
   const handleMenuIndex = (index) => {
     console.log("Bu menu indexi:", index);
   };
 
   return (
     <section className="container">
-
       <div className="grid grid-cols-2 mt-[50px] gap-8">
-
         <div className="">
           <img className="w-[300px]" src="./logo/wood_city_black.png" alt="" />
 
-          <p className="font-bold text-[60px] mt-[25px]">Готовые модульные дома и бани</p>
+          <p className="font-bold text-[60px] mt-[25px]">
+            Готовые модульные дома и бани
+          </p>
 
-          <p className="text-[30px] mt-[25px]">Загородная жизнь с комфортом <span className="font-bold">от 518 000 рублей</span> с установкой за 1 день</p>
+          <p className="text-[30px] mt-[25px]">
+            Загородная жизнь с комфортом{" "}
+            <span className="font-bold">от 518 000 рублей</span> с установкой за
+            1 день
+          </p>
 
           <div className="mt-[50px]">
             <button className="btn mr-[25px]">Оставить заявку</button>
@@ -27,17 +30,21 @@ function WoodenHouses() {
           </div>
 
           <div className="mt-[40px] text-[20px]">
-            <p>Получите готовый модульный дом или баню с установкой за 1 день и доставкой по всей России</p>
+            <p>
+              Получите готовый модульный дом или баню с установкой за 1 день и
+              доставкой по всей России
+            </p>
           </div>
-
         </div>
 
         <div className="">
           <video className="w-full h-auto" muted loop autoplay controls>
-            <source src="./video/DP-Module - Модульные дома и бани.mp4" type="video/mp4" />
+            <source
+              src="./video/DP-Module - Модульные дома и бани.mp4"
+              type="video/mp4"
+            />
           </video>
         </div>
-
       </div>
 
       <div className="mt-[25px]">
@@ -50,21 +57,29 @@ function WoodenHouses() {
         <ScrollableMenu onMenuIndex={handleMenuIndex} />
       </div>
 
-
       <div className="grid grid-cols-2 gap-4 my-[50px]">
 
-        <div className="border rounded-lg grid grid-cols-1">
+        <div className="border rounded-lg grid grid-cols-1 p-2">
+          
+          <motion.div
+            className="relative"
+            whileHover={{ scale: 1.0 }}
+            whileTap={{ scale: 0.95 }}
+            transition={{ type: "spring", stiffness: 400, damping: 10 }}
+          >
+            <img
+              className="h-[450px] w-full object-cover"
+              src="https://img.freepik.com/free-photo/3d-rendering-wooden-house_23-2151264506.jpg"
+              alt=""
+            />
 
-          <motion.div className="relative" whileHover={{ scale: 1.00 }} whileTap={{ scale: 0.950 }} transition={{ type: "spring", stiffness: 400, damping: 10 }}>
-
-            <img className="h-[450px] w-full object-cover" src="https://img.freepik.com/free-photo/3d-rendering-wooden-house_23-2151264506.jpg" alt="" />
-            
-            <div  className="absolute top-5 left-5 border border-red-700 group hover:top-0 hover:left-0 hover:w-full">
-            <img className="w-[200px] group-hover:w-full group-hover:h-[450px] object-cover" src="https://optim.tildacdn.com/stor6532-6434-4933-b537-613962363530/-/format/webp/99134150.jpg" alt="" />
+            <div className="absolute top-5 left-5 border border-red-700 group hover:top-0 hover:left-0 hover:w-full">
+              <img
+                className="w-[200px] group-hover:w-full group-hover:h-[450px] object-cover"
+                src="https://optim.tildacdn.com/stor6532-6434-4933-b537-613962363530/-/format/webp/99134150.jpg"
+                alt=""
+              />
             </div>
-
-
-            
           </motion.div>
 
           <div>
@@ -84,9 +99,12 @@ function WoodenHouses() {
         </div>
 
         <div className="border rounded-lg grid grid-cols-1">
-
           <div className="group">
-            <img className="h-[450px] w-full object-cover" src="https://optim.tildacdn.com/stor6663-3264-4630-b536-353537643736/-/format/webp/76691465.jpg" alt="" />
+            <img
+              className="h-[450px] w-full object-cover"
+              src="https://optim.tildacdn.com/stor6663-3264-4630-b536-353537643736/-/format/webp/76691465.jpg"
+              alt=""
+            />
             {/* <img className="w-[200px]" src="https://optim.tildacdn.com/stor6532-6434-4933-b537-613962363530/-/format/webp/99134150.jpg" alt="" /> */}
             <div className="relative">
               <div className="absolute top-0 left-0 z-[1234] bg-red-400 w-full h-full"></div>
@@ -106,14 +124,9 @@ function WoodenHouses() {
             <button className="btn mr-2">Оставить заявку</button>
             <button className="btn ml-2">Подробнее →</button>
           </div>
-
         </div>
 
       </div>
-
-
-
-
     </section>
   );
 }
