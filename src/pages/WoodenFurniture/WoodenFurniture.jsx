92d158f8458701698ac3372cab7c8687f151c942
shahbozshirinboyev <shahboz.sh.b@gmail.com>
2024-10-11 13:27:23 +0500

    Done Wooden furniture page

--- src/pages/WoodenFurniture/WoodenFurniture.jsx
+++ src/pages/WoodenFurniture/WoodenFurniture.jsx
@@ -1,14 +1,14 @@
 import { useState, useRef, useEffect } from "react";
 import { woodenFurniture } from "../../data/data";
 
-import PhoneInput from 'react-phone-input-2'
-import 'react-phone-input-2/lib/style.css'
+import PhoneInput from "react-phone-input-2";
+import "react-phone-input-2/lib/style.css";
 
 // react-hot-toast
-import { Toaster, toast } from 'react-hot-toast';
+import { Toaster, toast } from "react-hot-toast";
 
 // http
-import http from '../../services/http'
+import http from "../../services/http";
 
 //images
 import furniture_1 from "../../../public/furniture/6.jpg";
@@ -67,78 +67,59 @@ function WoodenFurniture() {
   };
 
   // -----------------------order
-    const [nameValue, setNameValue] = useState('');
-    const [phoneValue, setPhoneValue] = useState('');
-    const [closeModalId, setCloseModalId] = useState('');
-    const [activeCardInfo, setActiveCardInfo] = useState([]);
+  const [nameValue, setNameValue] = useState("");
+  const [phoneValue, setPhoneValue] = useState("");
+  const [closeModalId, setCloseModalId] = useState("");
+  const [activeCardInfo, setActiveCardInfo] = useState([]);
 
-    const handleSubmit = (e) => {
-        e.preventDefault();
-        console.log(activeCardInfo)
-        // Ism va telefon raqamini tekshirish
-        if (!nameValue) {
-            toast.error("Ismingizni kiriting!")
-            // console.log(closeModalId)
-            return;
-        }
-        if (!phoneValue) {
-            toast.error("Telefon raqamingizni kiriting!")
-            return;
-        }
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    console.log(activeCardInfo);
+    // Ism va telefon raqamini tekshirish
+    if (!nameValue) {
+      toast.error("Ismingizni kiriting!");
+      // console.log(closeModalId)
+      return;
+    }
+    if (!phoneValue) {
+      toast.error("Telefon raqamingizni kiriting!");
+      return;
+    }
 
-        const formData = new FormData();
-        const photo = `${window.location.origin}${activeCardInfo.card.image}`;
-        formData.append("chat_id", "-1002294640036");
-        formData.append("photo", photo);
-        formData.append("caption", 
-          `
-          Order: ${activeCardInfo.card.title}
-          👤Имя: ${nameValue}
-          📱Телефон: +${phoneValue}
-          `
-        ); 
-        console.log("photo url: " + photo)
+    const photo = `${window.location.origin}${activeCardInfo.card.image}`;
 
-        toast.promise(
-              http.post('/sendPhoto', formData),
-                // {
-                //   chat_id: "660100854",
-                //   chat_id: "-1002294640036",
-                // yangi chat ID olish uchun so'rov yuborish kerak: https://api.telegram.org/bot8110745041:AAGgqllrE9mwsCkQK8mhFsiG2quMGJHkD8I/getUpdates
-                //                                                  https://api.telegram.org/bot${token}/getUpdates
-                //   text: `
-                //   Запрос отложен:
+    toast.promise(
+      http.post("/sendPhoto", {
+        chat_id: "-1002294640036",
+        photo: photo,
+        caption: ` Запрос отложен:\n\nOrder: ${activeCardInfo.card.title}\n👤Имя: ${nameValue}\n📱Телефон: +${phoneValue}`,
+      }),
+      {
+        loading: "Отправка сообщения...", // Kutish holati
+        success: (response) => {
+          closeModal();
+          console.log(response); // Muvaffaqiyatli natija
+          return <b>Сообщение успешно отправлено!</b>;
+        },
+        error: (error) => {
+          console.log(error); // Xato holati
+          // console.log(photo)
+          return <b>Не удалось отправить сообщение.</b>;
+        },
+      }
+    );
+  };
 
-                //   👤Имя: ${nameValue}
-                //   📱Телефон: +${phoneValue}`
-                // }
-              
-              {
-                loading: 'Отправка сообщения...', // Kutish holati
-                success: (response) => {
-                    closeModal();
-                  console.log(response); // Muvaffaqiyatli natija
-                  return <b>Сообщение успешно отправлено!</b>;
-                },
-                error: (error) => {
-                  console.log(error); // Xato holati
-                  return <b>Не удалось отправить сообщение.</b>;
-                },
-              }
-            );
-    };
-
-    const closeModal = () => {
-        const modal = document.getElementById(closeModalId);
-        modal.close(); // Modalni yopish
-        setNameValue(''); // Ism inputini tozalash
-        setPhoneValue('998'); // Telefon raqami inputini tozalash
-        // setCloseModalId('');
-    };
+  const closeModal = () => {
+    const modal = document.getElementById(closeModalId);
+    modal.close(); // Modalni yopish
+    setNameValue(""); // Ism inputini tozalash
+    setPhoneValue("998"); // Telefon raqami inputini tozalash
+    // setCloseModalId('');
+  };
 
   return (
     <section className="container mb-[25px] text-[#160A06]">
-
       <div>
         <p className="font-bold text-[32px] text-center my-[25px]">
           Wooden furniture
@@ -146,7 +127,6 @@ function WoodenFurniture() {
       </div>
 
       <div className="grid grid-cols-2 gap-4 md:grid-cols-3">
-
         <div className="col-span-2 row-span-2 bg-beige">
           <img src={furniture_1} alt="" className="w-full h-full" />
         </div>
@@ -166,7 +146,6 @@ function WoodenFurniture() {
             className="absolute top-0 left-0 w-full h-full object-cover  blur-[0px]"
           />
         </div>
-
       </div>
 
       <div className="mt-[25px]">
@@ -211,7 +190,7 @@ function WoodenFurniture() {
 
           <div
             className={`absolute top-0 right-0 h-full w-[100px] bg-gradient-to-l from-base-200 to-transparent flex items-center justify-end px-2 ${
-            scrollPos < maxScroll ? "flex" : "hidden"
+              scrollPos < maxScroll ? "flex" : "hidden"
             }`}
           >
             <i
@@ -223,175 +202,206 @@ function WoodenFurniture() {
       </>
       {/* Scroll Navigation links END */}
 
-
       {/* Cards START */}
       <div className="grid grid-cols-1 gap-3 py-10 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4">
-
         {getCounts(activeMenuBtn).map((card) => (
-
           <div key={card.title} className="rounded-[10px] border bg-white">
-            
             <div className="p-3">
-              <img 
-                onClick={() => document.getElementById(`info_${card.id}`).showModal()}
-                src={card.image} alt={card.title} className="cursor-pointer object-cover object-center h-[250px] w-full rounded-[10px]"
+              <img
+                onClick={() =>
+                  document.getElementById(`info_${card.id}`).showModal()
+                }
+                src={card.image}
+                alt={card.title}
+                className="cursor-pointer object-cover object-center h-[250px] w-full rounded-[10px]"
               />
             </div>
 
             <div className="p-3">
-
-              <p className="font-semibold text-[18px] pl-2 mb-2">{card.title}</p>
+              <p className="font-semibold text-[18px] pl-2 mb-2">
+                {card.title}
+              </p>
 
               <span className="text-[20px] text-sky-500 font-semibold mb-2 block">
-                <span className="font-bold pl-2">$</span> &nbsp;{card.price} 
+                <span className="font-bold pl-2">$</span> &nbsp;{card.price}
               </span>
 
               <div className="grid grid-cols-2 gap-4 pt-4">
-                <button className="btn" onClick={() => {document.getElementById(`order_${card.id}`).showModal(); setCloseModalId(`order_${card.id}`); setActiveCardInfo({card})}} >
+                <button
+                  className="btn"
+                  onClick={() => {
+                    document.getElementById(`order_${card.id}`).showModal();
+                    setCloseModalId(`order_${card.id}`);
+                    setActiveCardInfo({ card });
+                  }}
+                >
                   Оставить заявку
                 </button>
-                <button className="btn" onClick={() => document.getElementById(`info_${card.id}`).showModal()}>
+                <button
+                  className="btn"
+                  onClick={() =>
+                    document.getElementById(`info_${card.id}`).showModal()
+                  }
+                >
                   Подробнее →
                 </button>
               </div>
-              
             </div>
 
             {/* Modal START */}
 
             {/* Modal Info */}
             <dialog id={`order_${card.id}`} className="modal">
+              <Toaster />
 
-            <Toaster />
-
-              <div className="modal-box w-11/12 max-w-xl p-0 ">             
-
-              {/* Modal header Start */}
-              <form method="dialog" className="border-b-[2px] border-base-200 h-[60px] grid grid-cols-2 items-center px-[24px] bg-custom-green-10">
-                  <span className="text-custom-green-dark font-bold">Оставить заявку</span>
+              <div className="modal-box w-11/12 max-w-xl p-0 ">
+                {/* Modal header Start */}
+                <form
+                  method="dialog"
+                  className="border-b-[2px] border-base-200 h-[60px] grid grid-cols-2 items-center px-[24px] bg-custom-green-10"
+                >
+                  <span className="text-custom-green-dark font-bold">
+                    Оставить заявку
+                  </span>
                   <div className="text-end">
-                  <button className="btn btn-sm border-0 btn-circle text-custom-green-dark bg-custom-green-10 hover:bg-custom-green-30"> ✕ </button>
+                    <button className="btn btn-sm border-0 btn-circle text-custom-green-dark bg-custom-green-10 hover:bg-custom-green-30">
+                      {" "}
+                      ✕{" "}
+                    </button>
                   </div>
-              </form>
-              {/* Modal header End */}
+                </form>
+                {/* Modal header End */}
 
-              <div className="p-4">
-
-                <div className="px-6 mb-4">
-                  <p className="font-bold">Ваш заказ:</p>
-                  <div className="flex gap-8 p-2">
-                    <div>
-                      <img className="w-[100px] h-[100px] object-cover" src={card.image} alt="" />
+                <div className="p-4">
+                  <div className="px-6 mb-4">
+                    <p className="font-bold">Ваш заказ:</p>
+                    <div className="flex gap-8 p-2">
+                      <div>
+                        <img
+                          className="w-[100px] h-[100px] object-cover"
+                          src={card.image}
+                          alt=""
+                        />
+                      </div>
+                      <div className="">
+                        <p className="font-semibold text-[20px]">
+                          {card.title}
+                        </p>
+                        <p className="font-bold text-[16px] opacity-70">
+                          $ {card.price}
+                        </p>
+                        <p>Размер: {card.size} см</p>
+                      </div>
                     </div>
-                    <div className="">
-                      <p className="font-semibold text-[20px]">{card.title}</p>
-                      <p className="font-bold text-[16px] opacity-70">$ {card.price}</p>
-                      <p>Размер: {card.size} см</p>
-                    </div>
-
                   </div>
-                </div>
 
                   {/* <p className="text-center py-4 font-bold text-[20px] md:text-[22px] lg:text-[24px] xl:text-[26px]">Заказать обратный звонок</p> */}
 
-                  
-
-                  <form action="" className="px-6" onSubmit={handleSubmit} >
-
-                  <label className="form-control w-full mb-2">
-
+                  <form action="" className="px-6" onSubmit={handleSubmit}>
+                    <label className="form-control w-full mb-2">
                       <div className="label">
-                          <span className="label-text">Ваше имя:</span>
-                          {/* <span className="label-text-alt">Top Right label</span> */}
+                        <span className="label-text">Ваше имя:</span>
+                        {/* <span className="label-text-alt">Top Right label</span> */}
                       </div>
 
-                      <input 
-                      value={nameValue} // Ism qiymatini boshqarish
-                      onChange={(e) => setNameValue(e.target.value)} // Ism o'zgarganda yangilash 
-                      type="text" 
-                      // required
-                      placeholder="Ваше имя" 
-                      className="input input-bordered w-full"
-                      style={{borderRadius: '.25rem', height: '45px', fontSize: '16px',}}
+                      <input
+                        value={nameValue} // Ism qiymatini boshqarish
+                        onChange={(e) => setNameValue(e.target.value)} // Ism o'zgarganda yangilash
+                        type="text"
+                        // required
+                        placeholder="Ваше имя"
+                        className="input input-bordered w-full"
+                        style={{
+                          borderRadius: ".25rem",
+                          height: "45px",
+                          fontSize: "16px",
+                        }}
                       />
+                    </label>
 
-                  </label>
-
-                  <label className="form-control w-full mb-2">
-
+                    <label className="form-control w-full mb-2">
                       <div className="label">
-                          <span className="label-text">Ваше номер телефона:</span>
-                          {/* <span className="label-text-alt">Top Right label</span> */}
+                        <span className="label-text">Ваше номер телефона:</span>
+                        {/* <span className="label-text-alt">Top Right label</span> */}
                       </div>
-                      
+
                       <PhoneInput
-                          value={phoneValue} // Telefon raqami qiymatini boshqarish
-                          onChange={setPhoneValue} // Telefon raqami o'zgarganda yangilash
-                          country={'uz'}
-                          onlyCountries={['uz', 'kz', 'kg', 'tj', 'tm']}
-                          masks={{
-                              uz: '(..) ...-..-..', // O'zbekiston
-                              kz: '(...) ...-..-..', // Qozog'iston
-                              kg: '(..) ...-..-..', // Qirg'iziston
-                              tj: '(..) ...-..-..', // Tojikiston
-                              tm: '(..) ..-..-..', // Turkmaniston
-                              }}
-                          inputClass="input input-bordered w-full"
-                          inputStyle={{ width: '100%', height: '45px', border: '1px solid #ccc', borderRadius: '.25rem', transition: 'border-color 0.2s', fontSize: '16px', }}
-                          inputProps={{ name: 'phone', required: true }}
+                        value={phoneValue} // Telefon raqami qiymatini boshqarish
+                        onChange={setPhoneValue} // Telefon raqami o'zgarganda yangilash
+                        country={"uz"}
+                        onlyCountries={["uz", "kz", "kg", "tj", "tm"]}
+                        masks={{
+                          uz: "(..) ...-..-..", // O'zbekiston
+                          kz: "(...) ...-..-..", // Qozog'iston
+                          kg: "(..) ...-..-..", // Qirg'iziston
+                          tj: "(..) ...-..-..", // Tojikiston
+                          tm: "(..) ..-..-..", // Turkmaniston
+                        }}
+                        inputClass="input input-bordered w-full"
+                        inputStyle={{
+                          width: "100%",
+                          height: "45px",
+                          border: "1px solid #ccc",
+                          borderRadius: ".25rem",
+                          transition: "border-color 0.2s",
+                          fontSize: "16px",
+                        }}
+                        inputProps={{ name: "phone", required: true }}
                       />
-                      
-                  </label>
+                    </label>
 
-                  <p className='py-2 text-start text-[14px] lg:text-[16px]'>
-                  Специалист компании свяжется с вами в ближайшее время, а на вашу почту будет отправлена презентация проекта для ознакомления.
-                  </p>
+                    <p className="py-2 text-start text-[14px] lg:text-[16px]">
+                      Специалист компании свяжется с вами в ближайшее время, а
+                      на вашу почту будет отправлена презентация проекта для
+                      ознакомления.
+                    </p>
 
-                  <button className="btn my-4 w-full" >Отправить</button>
-
+                    <button className="btn my-4 w-full">Отправить</button>
                   </form>
-
-              </div>
-
+                </div>
               </div>
 
               {/* Outside close section start */}
-              <form method="dialog" className="modal-backdrop"><button>close</button></form>
+              <form method="dialog" className="modal-backdrop">
+                <button>close</button>
+              </form>
               {/* Outside close section end */}
-
             </dialog>
 
             {/* Order Modal */}
             <dialog id={`info_${card.id}`} className="modal">
               <div className="modal-box w-11/12 max-w-5xl p-0">
-
                 {/* Modal header Start */}
-                <form method="dialog" className="border-b-[2px] border-base-200 h-[60px] grid grid-cols-2 items-center px-[24px] bg-custom-green-10">
-                  <span className="text-custom-green-dark font-bold">Подробнее →</span>
+                <form
+                  method="dialog"
+                  className="border-b-[2px] border-base-200 h-[60px] grid grid-cols-2 items-center px-[24px] bg-custom-green-10"
+                >
+                  <span className="text-custom-green-dark font-bold">
+                    Подробнее →
+                  </span>
                   <div className="text-end">
-                    <button className="btn btn-sm border-0 btn-circle text-custom-green-dark bg-custom-green-10 hover:bg-custom-green-30"> ✕ </button>
+                    <button className="btn btn-sm border-0 btn-circle text-custom-green-dark bg-custom-green-10 hover:bg-custom-green-30">
+                      {" "}
+                      ✕{" "}
+                    </button>
                   </div>
                 </form>
                 {/* Modal header End */}
 
-                
-                  <FurnitureProductInfo />
-                
-
+                <FurnitureProductInfo />
               </div>
               {/* Outside close section start */}
-              <form method="dialog" className="modal-backdrop"><button>close</button></form>
+              <form method="dialog" className="modal-backdrop">
+                <button>close</button>
+              </form>
               {/* Outside close section end */}
             </dialog>
 
             {/* Modal END */}
-
           </div>
         ))}
-
       </div>
       {/* Cards END */}
-
     </section>
   );
 }
